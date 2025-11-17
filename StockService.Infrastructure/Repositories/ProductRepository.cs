ecfaf02cefc297db6317643a52dfd48b6830619c
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-11-17 13:40:26 -0300

    FEAT: implements IProductRepository interface

--- StockService.Infrastructure/Repositories/ProductRepository.cs
+++ StockService.Infrastructure/Repositories/ProductRepository.cs
@@ -2,11 +2,50 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StockService.Domain.Entities;
+using StockService.Domain.Interfaces;
+using StockService.Infrastructure.Data;
 
 namespace StockService.Infrastructure.Repositories
 {
-    public class ProductRepository
+    public class ProductRepository : IProductRepository
     {
-        
+        private readonly StockServiceContext _context;
+        public ProductRepository(StockServiceContext context)
+        {
+            _context = context;
+        }
+        public async Task<Product> Create(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> Delete(Guid id)
+        {
+            var product = await GetById(id);
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<IEnumerable<Product>> GetAll()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product> GetById(Guid id)
+        {
+            return await _context.Products.Where(p => p.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<Product> Update(Product product)
+        {
+            _context.Entry(product).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return product;
+        }
     }
 }
