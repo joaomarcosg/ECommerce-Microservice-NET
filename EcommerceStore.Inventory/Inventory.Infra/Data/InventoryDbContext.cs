750da34b834911042eb7cbbecdee4a6d2f0bdae9
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-11-22 21:48:02 -0300

    FEAT: finishes Inventory DbContext

--- EcommerceStore.Inventory/Inventory.Infra/Data/InventoryDbContext.cs
+++ EcommerceStore.Inventory/Inventory.Infra/Data/InventoryDbContext.cs
@@ -2,12 +2,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Inventory.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Inventory.Infra.Data
 {
     public class InventoryDbContext : DbContext
     {
-        
+        public InventoryDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().Property(p => p.Name).HasMaxLength(100).IsRequired();
+            modelBuilder.Entity<Product>().Property(p => p.Description).HasMaxLength(255);
+            modelBuilder.Entity<Product>().Property(p => p.Price).HasPrecision(18, 2).IsRequired();
+        }
     }
 }
